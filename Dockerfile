8bc7e071b6db5fc6be3d23bb602302c200dd232f
hasantha.alahakoon <hasantha.alahakoon@cambio.se>
2019-01-15 12:48:35 +0530

    Added support for customizing server port and eureka server info

--- Dockerfile
+++ Dockerfile
@@ -18,6 +18,10 @@ ENV test_module_artifact_id=NO_TEST_MODULE_ARTIFACT_ID_PROVIDED
 
 ENV test_module_version=NO_TEST_MODULE_VERSION_PROVIDED
 
+ENV server_port=NO_SERVER_PORT_PROVIDED
+
+ENV eureka_server_port=EUREKA_SERVER_URL_PORT_NOT_PROVIDED
+
 COPY mvn/settings.xml /usr/share/maven/conf/settings.xml
 
 COPY SlaveTestService /home/aft/SlaveTestService
--- scripts/start_test_slave.sh
+++ scripts/start_test_slave.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export DISPLAY=:20
 Xvfb :20 -screen 0 1366x768x16 &
-mvn -Dtest_module_group_id=${test_module_group_id} -Dtest_module_artifact_id=${test_module_artifact_id} -Dtest_module_version=${test_module_version} spring-boot:run
+mvn -Dtest_module_group_id=${test_module_group_id} -Dtest_module_artifact_id=${test_module_artifact_id} -Dtest_module_version=${test_module_version} -Dserver.port=${server_port} -Deureka.client.serviceUrl.defaultZone=${eureka_server_port}/eureka/ spring-boot:run
 
