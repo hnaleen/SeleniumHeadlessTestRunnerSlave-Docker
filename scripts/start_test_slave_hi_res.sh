dddda7a32e252208d561e21cbaf831056f58f680
hasantha.alahakoon <hasantha.alahakoon@cambio.se>
2019-02-04 20:25:51 +0530

    Provided an alternative startup script which containes higer resolution display

--- scripts/start_test_slave_hi_res.sh
+++ scripts/start_test_slave_hi_res.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-Xvfb :99 &
-x11vnc & -display :99
+/opt/bin/entry_point.sh &
 mvn -Dtest_module_group_id=${test_module_group_id} -Dtest_module_artifact_id=${test_module_artifact_id} -Dtest_module_version=${test_module_version} -Dserver.port=${server_port} -Deureka.client.serviceUrl.defaultZone=${eureka_server_port}/eureka/ spring-boot:run
 
